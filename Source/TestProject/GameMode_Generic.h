4866262094a4d5696c4592f77550dc6c5a03c98a
leorchr <leogravillon@hotmail.fr>
2024-09-24 17:02:25 +0200

    [add] shoot

---
+++ Source/TestProject/GameMode_Generic.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "GameMode_Generic.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTPROJECT_API AGameMode_Generic : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TestProject/GameState_Generic.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "GameState_Generic.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTPROJECT_API AGameState_Generic : public AGameState
+{
+	GENERATED_BODY()
+	
+};
